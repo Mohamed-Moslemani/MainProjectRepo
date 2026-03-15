b660b79e9cc5ffe88c0ae7cd27597be1ec30281f
mohamed-moslemani <moslemanomohamed@gmail.com>
2026-03-15 18:05:04 +0200

    mohamedmoslemani/init/init files for structure

---
+++ services/gateway/app/models/__init__.py
@@ -0,0 +1,11 @@
+from .user import User
+from .case import Case
+from .document import Document
+from .ocr_result import OCRResult
+from .face_result import FaceResult
+from .payment import Payment
+from .audit_log import AuditLog
+from .password_reset import PasswordResetToken
+from .email_verification import EmailVerificationToken
+
+__all__ = ["User", "Case", "Document", "OCRResult", "FaceResult", "Payment", "AuditLog", "PasswordResetToken", "EmailVerificationToken"]
